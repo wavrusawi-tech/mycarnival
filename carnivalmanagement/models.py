07d8200c1998a4bf94935a3e33f08663b5e6a483
William <wavrusawi@gmail.com>
2026-06-01 13:41:48 -0500

    Added model

--- carnivalmanagement/models.py
+++ carnivalmanagement/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Carnival(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    date = models.DateField()
