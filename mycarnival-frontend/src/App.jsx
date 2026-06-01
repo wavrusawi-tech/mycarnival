c498e2dc9b77baf66da5621921ace5062944f689
William <wavrusawi@gmail.com>
2026-06-01 14:06:14 -0500

    Naming error

--- mycarnival-frontend/src/App.jsx
+++ mycarnival-frontend/src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import HomePage from './pages/HomePage'
+import WelcomePage from './pages/WelcomePage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main>
-      <HomePage />
+      <WelcomePage />
     </main>
   )
 }
