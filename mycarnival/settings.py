b85971b33fe9eee2e4e9e837176029759ca2e39c
William <wavrusawi@gmail.com>
2026-06-01 13:44:18 -0500

    Updated settings.py

--- mycarnival/settings.py
+++ mycarnival/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Allow React development server (usually port 5173) to access the API
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
