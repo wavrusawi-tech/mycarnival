a0b66d6f3299acdf0d0f83f33601345647d7e4f9
William <wavrusawi@gmail.com>
2026-06-01 14:04:31 -0500

    Added new page

---
+++ mycarnival-frontend/src/pages/WelcomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const WelcomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to myCarnival</h1>
+      <p>Before we get started, please choose your role:</p>
+      <button>Carnival Organizer</button>
+      <button>Attendee</button>
+    </div>
+  );
+};
+
+export default WelcomePage;
