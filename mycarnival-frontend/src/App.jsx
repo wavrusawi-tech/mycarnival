ce7ed1696ed4e53dbc129a44c90adb6601b6f2ca
William <wavrusawi@gmail.com>
2026-06-02 13:09:51 -0500

    Added 2 new pages

--- mycarnival-frontend/src/App.jsx
+++ mycarnival-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import WelcomePage from './pages/WelcomePage.jsx'
+import OrganizerCarnivalSelect from './pages/OrganizerCarnivalSelect.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,8 @@ function App() {
   return (
     <main>
       <WelcomePage />
+      <OrganizerCarnivalSelect />
+      <AttendeeCarnivalSelect />
     </main>
   )
 }
---
+++ mycarnival-frontend/src/pages/OrganizerCarnivalSelect.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrganizerCarnivalSelect = () => {
+    return (
+        <div>
+            <p>Coming Soon</p>
+        </div>
+    )
+}
+
+export default OrganizerCarnivalSelect;
